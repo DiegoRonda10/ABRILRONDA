d65d057905337308e611111755b7386512e7c576
dam <dam@203P30>
2025-04-10 11:31:13 +0200

    Merge master into rama
    Conflicts:
    	src/prueba2control/Prueba2control.java

---
+++ src/abrilronda/Clase2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package abrilronda;
+
+/**
+ *
+ * @author dam
+ */
+public class Clase2 {
+    public void visualizarRama(){
+            System.out.println("Estamos en la rama PRODUCCION");
+        }
+    
+    public void visualizarNombre(){
+            System.out.println("Diego Ronda Gonzalez");
+        }
+    
+}
