1fbbdd37c18c325975614e3dda2d114cca8ecc8d
dam <dam@203P30>
2025-04-10 11:26:43 +0200

    Merge master into rama
    Conflicts:
    	src/prueba2control/Prueba2control.java

---
+++ src/abrilronda/Clase1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package abrilronda;
+
+/**
+ *
+ * @author dam
+ */
+public class Clase1 {
+    public void visualizarNombre(){
+            System.out.println("Diego Ronda Gonzalez");
+        }
+    
+}
